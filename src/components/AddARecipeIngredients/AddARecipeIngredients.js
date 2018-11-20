9325b057b5580ee9357eff2865070eadaee6e776
William Harrison <wm.harrison@gmail.com>
2018-11-19 21:36:30 -0600

    working on add a recipe ingredients component. can add and remove ingredients.

--- src/components/AddARecipeIngredients/AddARecipeIngredients.js
+++ src/components/AddARecipeIngredients/AddARecipeIngredients.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { EventEmitter } from 'events';
 
 class AddARecipeIngredients extends Component {
 
     state = {
-        newIngredient: {
-            name: '',
-            quantity: 0,
-            id: 1,
             ingredients: [{ name: '', quantity: 0, id: 1 }]
-        }
     }
 
     componentDidMount() {
@@ -17,45 +13,72 @@ class AddARecipeIngredients extends Component {
         this.props.dispatch(action);
     }
 
-    handleChange = (event) => {
+    handleChange = (i) => (event) => {
         console.log(event.target.value);
-        this.setState({
-            newIngredient: {
-                ...this.state.newIngredient, [event.target.name]: event.target.value,
-            }
-        })
-    }
+        const newIngredients = this.state.ingredients.map((ingredient, si) => {
+            if (i !== si) return ingredient;
+            return { ...ingredient, [event.target.name]: event.target.value };
+         });
+        this.setState({ ingredients: newIngredients });
+        }
 
-    submitIngredients = (event) => {
+    // submitIngredients = (event) => {
+    //     event.preventDefault();
+    //     this.props.dispatch({ type: 'NEW_INGREDIENT', payload: this.state.newIngredient })
+    //     this.setState({
+    //         newIngredient: {
+    //             name: '',
+    //             quantity: '',
+    //             id: '',
+    //             }
+    //     });
+    //     this.clearInputFields();
+    // }
+
+    handleAddIngredient = (event) => {
         event.preventDefault();
-        this.props.dispatch({ type: 'NEW_INGREDIENT', payload: this.state.newIngredient })
         this.setState({
-            newIngredient: {
-                name: '',
-                quantity: '',
-                id: '',
-                }
+          ingredients: this.state.ingredients.concat([{ name: '', quantity: 0, id: 1 }])
         });
-        this.clearInputFields();
+      }
+
+    handleRemoveIngredient = (i) => (event) => {
+        event.preventDefault();
+        this.setState({
+            ingredients: this.state.ingredients.filter((s, si) => i !== si)
+        });
     }
 
     clearInputFields = () => {
-        this.setState( this.newIngredient );
+        this.setState( this.ingredient );
+    }
+
+    logIngredientState = (event) => {
+        event.preventDefault();
+        console.log(this.state.ingredients);
     }
 
     render() {
         return (
             <div>
-                {JSON.stringify(this.props.reduxState.ingredientUnits)}
+                {JSON.stringify(this.state)}
                 <p>Add your recipe ingredients here.</p>
                 <form onSubmit={this.submitIngredients}>
-                {this.state.newIngredient.ingredients.map((ingredient, i) => (<>
-                <input type="text" name="name" value={this.state.newIngredient.name} onChange={this.handleChange}/>
-                <input type="number" name="quantity" value={this.state.newIngredient.quantity} onChange={this.handleChange}/>
-                <select required placeholder="Select Ingredient Measure" value={this.state.newIngredient.id} onChange={this.handleChange} name="id">{this.props.reduxState.ingredientUnits.map(name => (<option key={name.id} value={name.id}>{name.name}</option>))}</select></>
+                {this.state.ingredients.map((ingredient, i) => (
+                    <>
+                        <input type="text" name="name" placeholder="Enter Ingredient Name"
+                        value={ingredient.name} onChange={this.handleChange(i)}/>
+
+                        <input type="number" name="quantity" placeholder="Enter Ingredient Quantity"
+                        value={ingredient.quantity} onChange={this.handleChange(i)}/>
+
+                        <select required placeholder="Select Ingredient Measure"
+                        value={ingredient.id} onChange={this.handleChange(i)} name="id">{this.props.reduxState.ingredientUnits.map(name => (<option key={name.id} value={name.id}>{name.name}</option>))}</select>
+                        <button onClick={this.handleRemoveIngredient(i)}>Remove this ingredient</button>
+                    </>
                 ))}
                 <button type="button" onClick={this.handleAddIngredient}>Add another ingredient</button>
-                <button>Save Ingredient List</button>
+                <button onClick={this.logIngredientState}>Save Ingredient List</button>
                 </form>
             </div>
         );
