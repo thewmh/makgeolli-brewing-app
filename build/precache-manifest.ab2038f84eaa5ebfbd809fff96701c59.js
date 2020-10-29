1e0f71190a58179c32013b0a771380d2fe83b82b
William Harrison <wharrison@horizontal.com>
2020-10-29 12:10:32 -0500

    adds server.js, updates package.json, builds project

---
+++ build/precache-manifest.ab2038f84eaa5ebfbd809fff96701c59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f31f6d8632ee03cbda44",
+    "url": "/static/js/main.08e68a61.chunk.js"
+  },
+  {
+    "revision": "a5477d5f5c3f6751b8bb",
+    "url": "/static/js/2.9f8a724a.chunk.js"
+  },
+  {
+    "revision": "f31f6d8632ee03cbda44",
+    "url": "/static/css/main.1cacd209.chunk.css"
+  },
+  {
+    "revision": "a5477d5f5c3f6751b8bb",
+    "url": "/static/css/2.aa1f52c0.chunk.css"
+  },
+  {
+    "revision": "6b3938251c26cce2670582a918f06ce7",
+    "url": "/index.html"
+  }
+];
