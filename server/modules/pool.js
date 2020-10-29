2baedd60665b08499277efa91d011a1a484a09d5
William Harrison <wharrison@horizontal.com>
2020-10-29 08:29:46 -0500

    removes pool error console

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -41,9 +41,9 @@ const pool = new pg.Pool(config);
 
 // the pool with emit an error on behalf of any idle clients
 // it contains if a backend error or network partition happens
-pool.on('error', (err) => {
-  console.log('Unexpected error on idle client', err);
-  process.exit(-1);
-});
+// pool.on('error', (err) => {
+//   console.log('Unexpected error on idle client', err);
+//   process.exit(-1);
+// });
 
 module.exports = pool;
