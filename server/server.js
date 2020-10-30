f26f5609306063863811cbf9abc2ea9e97c6699b
William Harrison <wharrison@horizontal.com>
2020-10-30 09:35:43 -0500

    updates server/server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-
+const path= require("path");
 const app = express();
 const bodyParser = require('body-parser');
 const sessionMiddleware = require('./modules/session-middleware');
